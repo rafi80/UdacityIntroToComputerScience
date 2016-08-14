190f9c3be1c1ddeeca12b2a00c0117823b3a78dc
rafi80 <hlebionek@tlen.pl>
2016-08-14 21:50:20 +0200

    Final lesson 3

--- lesson3/numbers_in_lists.py
+++ lesson3/numbers_in_lists.py
@@ -11,6 +11,32 @@
 
 #Hint - "int()" turns a string's element into a number
 
+# def numbers_in_lists(string):
+#     result = []
+#     result.append(int(string[0]))
+#     i = 1
+#     sublist = []
+#     preceding_number = int(result[0])
+#     while i< len(string)-1:
+#         if int(string[i]) <= preceding_number:
+#             sublist.append(int(string[i]))
+#             preceding_number = int(string[i])
+#         else:
+#             if sublist != []:
+#                 result.append(sublist)
+#             sublist = []
+#             result.append(int(string[i]))
+#             preceding_number = int(string[i])
+#         i+=1
+#     if i == len(string)-1:
+#         if int(string[i]) <= preceding_number:
+#             sublist.append(int(string[i]))
+#             result.append(sublist)
+#         else:
+#             result.append(int(string[i]))
+#     return result
+
+# redoux
 def numbers_in_lists(string):
     result = []
     result.append(int(string[0]))
@@ -20,7 +46,8 @@ def numbers_in_lists(string):
     while i< len(string)-1:
         if int(string[i]) <= preceding_number:
             sublist.append(int(string[i]))
-            preceding_number = int(string[i])
+            if int(string[i]) > preceding_number:
+                preceding_number = int(string[i])
         else:
             if sublist != []:
                 result.append(sublist)
@@ -36,6 +63,8 @@ def numbers_in_lists(string):
             result.append(int(string[i]))
     return result
 
+
+
 #testcases
 string = '543987'
 result = [5,[4,3],9,[8,7]]
@@ -47,7 +76,7 @@ result = [9,[8,7,6,5,4,3,2,1]]
 print numbers_in_lists(string)
 print repr(string), numbers_in_lists(string) == result
 string = '455532123266'
-result = [4, 5, [5, 5, 3, 2, 1], 2, 3, [2], 6, [6]]
+result = [4, 5, [5, 5, 3, 2, 1, 2, 3, 2], 6, [6]]
 print numbers_in_lists(string)
 print repr(string), numbers_in_lists(string) == result
 string = '123456789'
