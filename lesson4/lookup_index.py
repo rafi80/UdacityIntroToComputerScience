bea6e48c7702bdb8032ffeb5cba73aee27621bed
rhlebion <rafal.hlebionek@capgemini.com>
2016-10-17 13:19:01 +0200

    add lookup exercise

---
+++ lesson4/lookup_index.py
@@ -0,0 +1,26 @@
+# Define a procedure, lookup,
+# that takes two inputs:
+
+# - an index
+# - keyword
+
+# The procedure should return a list
+# of the urls associated
+# with the keyword. If the keyword
+# is not in the index, the procedure
+# should return an empty list.
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+
+def lookup(index, keyword):
+    for element in index:
+        if element[0] == keyword:
+            return element[1]
+    return []
+
+
+print lookup(index, 'computing')
+print lookup(index, 'udacity')
+print lookup(index, 'stefan')
